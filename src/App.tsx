71e03c228aec2743b84e3356c32432e7eff7cf24
Abdullah <abdullahch433@gmal.com>
2023-08-10 21:38:58 +0500

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+//import Toast from "shared/components/toast";
+import AuthRoute from "shared/routes/authRoutes";
+//import { initSocket } from "shared/services/socketService";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    //initSocket();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthRoute />
+      {/* <Toast /> */}
     </div>
   );
 }
